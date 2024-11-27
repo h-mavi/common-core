bc4ca9660375ca472a22b1a0696fc03ff7779afa
Maria Vittoria Fanelli <mfanelli@c1r3p4.42firenze.it>
2024-11-27 15:40:39 +0100

    Libft

---
+++ Libft/libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfanelli <mfanelli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 13:36:11 by mfanelli          #+#    #+#             */
+/*   Updated: 2024/11/27 15:39:40 by mfanelli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_tolower(int value);
+int	ft_toupper(int value);
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+
+#endif
