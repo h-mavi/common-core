95423f49d43606491c2029c6af949df3368dd946
Maria Vittoria Fanelli <mfanelli@c1r3p2.42firenze.it>
2024-11-28 17:38:30 +0100

    main al primo es di lista

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: mfanelli <mfanelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 10:39:46 by mfanelli          #+#    #+#             */
-/*   Updated: 2024/11/28 15:26:13 by mfanelli         ###   ########.fr       */
+/*   Updated: 2024/11/28 17:34:30 by mfanelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,8 @@ void	ft_putchar_fd(char c, int fd)
 		return ;
 	write (fd, &c, 1);
 }
+
+/* int	main(void)
+{
+	ft_putchar_fd('c', 1);
+} */
