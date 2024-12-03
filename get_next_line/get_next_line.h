f2abd43c8fae6bd2eb23acdde3c1501199219798
Maria Vittoria Fanelli <mfanelli@c1r3p6.42firenze.it>
2024-12-03 14:36:34 +0100

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfanelli <mfanelli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 14:34:35 by mfanelli          #+#    #+#             */
+/*   Updated: 2024/12/03 14:35:40 by mfanelli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE.H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdarg.h>
+
+
+
+#endif
